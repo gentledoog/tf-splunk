3bef226e2fa3960173d65223bfaafad11a7bdbe8
gentledog <benwzhu@gmail.com>
2023-10-25 11:48:58 -0400

    remove dumplicate ouput

--- main.tf
+++ main.tf
@@ -172,7 +172,8 @@ variable "region" {
  
 variable "availability_zone" {
 }
- 
+
+/* 
 output "splunk_public_ip" {
   value = module.splunk.splunk_public_ip
 }
@@ -182,3 +183,4 @@ output "splunk_default_username" {
 output "splunk_default_password" {
   value = module.splunk.splunk_default_password
 }
+*/
