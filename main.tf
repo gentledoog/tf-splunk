eecf6dc338ee87d28f45f207ac77d739d9b789d3
gentledog <benwzhu@gmail.com>
2023-10-25 10:40:40 -0400

    add tf cloud code main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+module "splunk" {
+  source  = "nvibert/splunk/aws"
+  version = "1.2.0"
+}
+ 
+variable "region" {
+}
+ 
+variable "availability_zone" {
+}
+ 
+output "splunk_public_ip" {
+  value = module.splunk.splunk_public_ip
+}
+output "splunk_default_username" {
+  value = module.splunk.splunk_default_username
+}
+output "splunk_default_password" {
+  value = module.splunk.splunk_default_password
+}
